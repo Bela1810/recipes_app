30240064fe845b6fffd8d2a0f969e7051613bd21
Bela <belacaste3@gmail.com>
2025-03-05 19:29:13 -0500

    new icons and images

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -13,13 +13,24 @@ class HomeScreen extends StatelessWidget{
       body: SafeArea(
         child: GridView.count(
         crossAxisCount: 2,
+        childAspectRatio: 0.6,
         children: [
-          Recipe(),
-          Recipe(),
-          Recipe(),
-          Recipe(),
-          Recipe(),
-          Recipe()
+          Recipe(
+            name: "Spaguetti",
+            imageUrl: 'https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg/medium',
+          ),
+          Recipe(
+            name: "Baked Salmon",
+            imageUrl: "https://www.themealdb.com/images/media/meals/1548772327.jpg",
+          ),
+          Recipe(
+            name: "Fish tacos",
+            imageUrl: "https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg",
+          ),
+          Recipe(
+            name: "Honey Salmon",
+            imageUrl: "https://www.themealdb.com/images/media/meals/xxyupu1468262513.jpg",
+          ),
         ],
       )),
     );
--- lib/home/widgets/recipe.dart
+++ lib/home/widgets/recipe.dart
@@ -1,8 +1,12 @@
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Recipe extends StatelessWidget {
-  const Recipe({super.key});
+    final String name;
+    final String imageUrl;
+    const Recipe({super.key, required this.name, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
@@ -10,43 +14,120 @@ class Recipe extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _buildAvatarInfo(),
-          SizedBox(height: 12),
-          _buildRecipeImage()
+          _buildRecipeImage(),
+          _buildName(),
+          _buildCategory(),
         ],
       ),
     );
   }
 
-  Widget _buildAvatarInfo(){
+  Widget _buildCategory() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Row(
+        children: [
+          Text(
+            "SeadFood",
+            style: TextStyle(
+              fontSize: 15,
+              color: Color(0xFF9FA5C0),
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          Text(
+            "  ●  ",
+            style: TextStyle(
+              fontSize: 13,
+              color: Color(0xFF9FA5C0),
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          Text(
+            ">60 mins",
+            style: TextStyle(
+              fontSize: 13,
+              color: Color(0xFF9FA5C0),
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAvatarInfo() {
     return Row(
       children: [
-        SizedBox(
-          height: 42.0,
-          width: 42.0,
-          child: Image.network('https://img.freepik.com/vector-premium/icono-perfil-simple-color-blanco-icono_1076610-50204.jpg'),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(15.0),
+          child: SizedBox(
+            height: 42.0,
+            width: 42.0,
+            child: Image.network(
+              fit: BoxFit.fill,
+              'https://images.pexels.com/photos/590478/pexels-photo-590478.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            ),
+          ),
         ),
         Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8.0),
-          child: Text('Belitaa'),
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Text(
+            'Bela',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+            ),
+          ),
         ),
       ],
     );
   }
 
-  Widget _buildRecipeImage(){
-    return Expanded(
-      child : SizedBox(
-        height: 250,
-        child: Image.network('https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg/medium',
-        fit: BoxFit.cover)
+Widget _buildRecipeImage() {
+  return Padding(
+    padding: const EdgeInsets.only(top: 16.6, right: 4),
+    child: Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(16.0),
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          top: 25,
+          right: 25,
+          child: ImageFiltered(
+            imageFilter: ImageFilter.blur(sigmaX: 0, sigmaY: 0), // Desenfoque aplicado
+            child: Icon(
+              Icons.favorite_border, // Icono sin relleno
+              color: Colors.white,
+              size: 24.0,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
 
+
+  Widget _buildName(){
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0),
+      child: Text(
+        name,
+        style: TextStyle(fontSize: 22.0, color: Color.fromARGB(255, 0, 0, 0), fontWeight: FontWeight.bold
+        ),
       )
-      );
+    );
   }
-
-
 
 
 
